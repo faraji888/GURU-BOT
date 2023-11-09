e2dc5efbf0db76aafb7d93119076dcc6cba4da17
faraji888 <148947189+faraji888@users.noreply.github.com>
2023-11-09 13:49:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['212600446183', 'NARCISSTIC', true],
+  ['212600446183', 'XPLZ', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['212600446183','212600446183'] 
+global.prems = ['212600446183', '212600446183', '212600446183']
+global.allowed = ['212600446183', '212600446183', '212600446183']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,25 +50,25 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '# 𝐆𝐁𝐗 /#/ 𝑵𝑨𝑹𝑪𝑰𝑺𝑺𝑰𝑺𝑻𝑰𝑪 !💨❤>'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
+global.packname = 'GBX - NARCISSIST!' 
+global.author = '@Asligbx' 
+global.menuvid = 'https://telegra.ph/file/2c9a77a3a2570ebd34004.jpg'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
+global.dygp = 'wa.me//212600446183 CHATE ME 😚❤💨>'
+global.fgsc = 'ME NUMBER ! 😍 +212600446183' 
+global.fgyt = '*```wa.me//212600446183```*'
 global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.fglog = 'https://telegra.ph/file/5578f0db8a6586f8e4332.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✅'
-global.error = '❌' 
+global.wait = '*⌛_CHARGING..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '🤯'
+global.dmoji = '😳😍💋👄👅🔞🤤'
+global.done = '😋😊😍💋✅'
+global.error = '😂❌' 
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
